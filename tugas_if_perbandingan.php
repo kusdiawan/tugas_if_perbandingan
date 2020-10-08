c02838ef987353e0e1ed01b179396cc2e9b3b87e
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-08 12:05:31 +0700

    Add files via upload

---
+++ tugas_if_perbandingan.php
@@ -0,0 +1,14 @@
+<?php
+   $a = 6;
+   $b = 10;
+
+   if ($a > $b){
+   	echo $a . " lebih besar dari " . $b; 
+   } else if ($a < $b){
+   	echo $a . " lebih kecil dari " . $b;
+   }else if ($a == $b){
+   	echo $a . " lebih besar dari " . $b; 
+   } else{
+   	echo $a . " lebih kecil dari " . $b;
+   }
+?>
